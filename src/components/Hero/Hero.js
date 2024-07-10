ddc525220b51bdb6fe874b82f8864b70ca8ec62e
SUBHAMRANA <54814938+SUBHAMRANA@users.noreply.github.com>
2024-07-10 23:56:35 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ import {
 } from "../../styles/GlobalComponents";
 import { LeftSection } from "./HeroStyles";
 import Button from "react-bootstrap/Button";
-import ExampleDoc from "../../files/resume.pdf";
+import Resume from "../../files/resume.pdf";
 
 const Hero = (props) => (
   <>
@@ -17,7 +17,7 @@ const Hero = (props) => (
           My Personal Portfolio
         </SectionTitle>
         <SectionText>
-          Hi, I'm Ritik Bhatt. <br />
+          Hi, I'm Subham Rana. <br />
           Nice to meet you! I am a results-driven Software Developer with a year
           of hands-on experience, holding a degree from the esteemed Army
           Institute of Technology, Pune. My approach is characterized by quiet
@@ -25,7 +25,7 @@ const Hero = (props) => (
           my skills. <br />I am eager to contribute my skills and enthusiasm to
           dynamic projects that demand creativity and technical excellence.
         </SectionText>
-        <a href={ExampleDoc} download="MyExampleDoc" target="_blank">
+        <a href={Resume} download="Resume" target="_blank">
           <Button variant="outline-warning">Download Resume</Button>
         </a>
       </LeftSection>
