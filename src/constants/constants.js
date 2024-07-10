566e45fbe8884341f1cce6ea29ff359e986a08af
SUBHAMRANA <54814938+SUBHAMRANA@users.noreply.github.com>
2024-07-10 23:58:17 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,62 +2,23 @@ export const projects = [
   {
     title: "Chatify",
     description:
-      "Developed a feature-rich chat app with real-time messaging and integrated a webRTC-based video chat, enabling users to join designated rooms for face-to-face communication.",
+      "Developed a feature-rich chat app with real-time messaging and integrated a webRTC-based video chat, enabling users to connect for face-to-face communication.",
     image: "/images/3.png",
-    tags: ["MERN STACK", "ChakraUI"],
-    source: "https://github.com/bhattritik21/Chatify-App",
-    visit: "https://chatify-app-25vx.onrender.com",
+    tags: ["MERN STACK"],
+    source: "https://github.com/SUBHAMRANA/Video_Chat_App",
+    visit: "https://teams-clone-ms-engage.netlify.app/",
     id: 0,
   },
   {
-    title: "R Scanner",
+    title: " RScanner",
     description:
-      "Led the development of a cutting-edge MERN project with Open-AI Integration, focused on revolutionizing the resume modification and enhancement suggestions, thereby optimizing candidates chances of securing their desired positions.",
+      "Revolutionizing job applications with MERN Stack and OpenAI! Effortlessly tailor resumes with keyword matching, insights, and suggestions for standout resumes. Transform your application process today.",
     image: "/images/2.png",
-    tags: ["Open AI","ReactJS","NodeJS"],
-    source: "https://github.com/bhattritik21/Rscanner",
+    tags: ["MERN", "OpenAi", "Bootstrap"],
+    source: "https://github.com/SUBHAMRANA/Rscanner",
     visit: "https://rscanner.onrender.com/",
     id: 1,
-  },
-  {
-    title: " Snake Game",
-    description:
-      "JavaScript Advance Project. Made a snake game using pure JavaScript for the logic and CSS for the animations. Features-Customizable Speed, Highscore Tracking",
-    image: "/images/6.png",
-    tags: ["Html", "css", "Vanilla JS"],
-    source: "https://github.com/bhattritik21/snake-game",
-    visit: "https://bhattritik21.github.io/snake-game/",
-    id: 2,
-  },
-  {
-    title: "Kr1da",
-    description:
-      "It is strictly be focused on engaging the users with a sports-oriented marketplace and franchise.Integrated Realtime APIs to showcase Live match Scores and Latest Sports news, ensuring secure user authentication and MongoDB database management.",
-    image: "/images/1.png",
-    tags: ["MERN STACK"],
-    source: "https://github.com/bhattritik21/Kr1da-Final",
-    visit: "https://kr1da.netlify.app/",
-    id: 3,
-  },
-  {
-    title: "GameZone",
-    description:
-      " Its a Gaming Reviews Page.This project served as a valuable canvas for learning the basics of CSS, encompassing crucial concepts such as styling, layout, and responsiveness.",
-    image: "/images/5.png",
-    tags: ["Html", "css"],
-    source: "https://github.com/bhattritik21/Latest-PC-Games-Review",
-    visit: "https://bhattritik21.github.io/Latest-PC-Games-Review/",
-    id: 4,
-  },
-  {
-    title: " Bhatt Blogs",
-    description: "Check out my own Blog",
-    image: "/images/projects.jpeg",
-    tags: ["Html", "css"],
-    source: "https://github.com/bhattritik21/Bhatt-blogs.github.io",
-    visit: "https://bhattritik21.github.io/Bhatt-blogs.github.io/",
-    id: 5,
-  },
+  }
 ];
 
 export const TimeLineData = [
@@ -66,7 +27,7 @@ export const TimeLineData = [
   { year: 2022, text: "Started an Internship" },
   {
     year: 2022 - 23,
-    text: "Explpored the web dev world more, learened new frameworks and techs",
+    text: "Explpored the web dev world more, learned new frameworks and techs",
   },
-  { year: 2023, text: "Joined HHAExchange as an Associate Software Engineer" },
+  { year: 2023, text: "Joined Turtlemint as a Software Engineer" },
 ];
