6f52e2e18c79a6d3368e96d55b0eb3c143d4e8eb
SUBHAMRANA <54814938+SUBHAMRANA@users.noreply.github.com>
2024-07-10 23:58:50 +0530

    Update experience.js

--- src/constants/experience.js
+++ src/constants/experience.js
@@ -1,22 +1,42 @@
 export const experience = [
   {
-    title: "HHAExchange",
+    title: "Turtlemint",
     description:
-      "Collaborated with big team, reducing code conflicts & enhancing project delivery by 30%. Revitalized legacy UI for a 30% faster load, improved backend for more functionality. Active in Agile methodologies",
-    image: "/images/8.png",
-    tags: ["dot NET MVC", "C#", "SQL", "Bitbucket"],
-    role: "Associate Software Developer",
+      "At Turtlemint, I excel as a Software Engineer, specializing in designing responsive landing pages using React and TypeScript. I conduct effective A/B testing to optimize user engagement and develop robust APIs with Reactive Java and Spring Boot for seamless backend integration.",
+    image: "/images/7.png",
+    tags: ["React", "typescript", "SpringBoot", "Reactive Java", "Mongo"],
+    role: "Software Engineer",
     time: "Jul 2023 - Present",
     id: 0,
   },
   {
-    title: "Udchalo",
+    title: "GeeksforGeeks",
     description:
-      "Enhanced Udchalo Housing Landing page with a 20% rise in user satisfaction. Collaborated with a 4-member team, implemented a scalable UI architecture for a flight portal, elevating the overall platform's User Experience.",
-    image: "/images/7.png",
-    tags: ["ReactJS", "Git", "Figma"],
-    role: "Front End Developer Intern",
-    time: "Jan 2022 - Apr 2022",
+      "As a Teaching Assistant Intern at Geeks for Geeks, I guided students in understanding OOPs, DBMS, CNS, and data structures. I conducted live sessions on DSA topics, enhancing student comprehension and supporting their academic growth.",
+    image: "/images/9.jpeg",
+    tags: ["C++", "Java", "Computer Fundamentals"],
+    role: "Teaching Assistant",
+    time: "Mar 2023 - Jun 2023",
     id: 1,
   },
+  {
+    title: "Microsoft",
+    description:
+      "During my internship at Microsoft, I integrated CI information into the Playwright dashboard, enhancing team efficiency. I optimized the dashboard and supported CI/CD pipelines across platforms, contributing to streamlined development processes and improved workflow.",
+    image: "/images/8.png",
+    tags: ["React", "TypeScipt", "Azure", "CI/CD Platforms"],
+    role: "Software Engineer Intern",
+    time: "Jun 2022 - Aug 2022",
+    id: 2,
+  },
+  {
+    title: "Microsoft",
+    description:
+      "As an Engage Mentee at Microsoft, I led the development of a video-chat app, delivering the project ahead of schedule. I focused on optimizing video call setup and real-time communication using WebRTC, React, and Socket.io, ensuring a seamless user experience.",
+    image: "/images/10.webp",
+    tags: ["React", "WebRtc", "Node.js", "Socket.io"],
+    role: "Engage Mentee",
+    time: "Jun 2021 - July 2021",
+    id: 3,
+  }
 ];
